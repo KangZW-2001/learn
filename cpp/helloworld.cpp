278c20522be9a3c1a320d18d18db2b5241168bf5
kangzw-mac <1637750647@qq.com>
2023-12-28 22:54:40 +0800

    xxx

---
+++ cpp/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+template <typename T>
+void func(T value)
+{
+    std::cout << value << std::endl;
+}
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
